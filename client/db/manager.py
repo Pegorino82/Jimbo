bc08df705c5b01fb179b5be752b719d2befda6d8
Pegorino82 <eshkryabin@yandex.ru>
2018-04-08 17:39:14 +0300

    update

--- client/db/manager.py
+++ client/db/manager.py
@@ -18,9 +18,7 @@ def manager(response, user):
     try:
         action = response['action']
         print('mongo_db manager action->', action)
-        if action:
+        if action in actions:
             actions.get(action)(response, user)
-    except KeyError:
-        print('wrong action')
     except Exception as err:
         print('failed on db manager', err)
